921223d1bba92ed6e2c4b4101825692e6816fc7c
Luca Kredel <luca.kredel@cyperfection.de>
2017-11-21 17:32:19 +0100

    Add license text

--- Classes/Hooks/ItemsProcFunc.php
+++ Classes/Hooks/ItemsProcFunc.php
@@ -2,6 +2,24 @@
 
 namespace Cyz\CyzAddress\Hooks;
 
+/*
+ * This file is part of the "Cyz_Address" Extension for TYPO3 CMS.
+ * (c) 2017 Luca Kredel <luca.kredel@cyperfection.de>, cyperfection gmbh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 use TYPO3\CMS\Core\Utility\StringUtility;
 
 class ItemsProcFunc
