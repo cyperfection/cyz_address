ddea022c7a1b1e6d314b577046d8c10333e8507d
Luca Kredel <luca.kredel@cyperfection.de>
2017-11-24 15:00:38 +0100

    Add License text and update description

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,24 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
+/*
+ * This file is part of the "Cyz_Address" Extension for TYPO3 CMS.
+ * (c) 2017 Luca Kredel <luca.kredel@cyperfection.de>, cyperfection gmbh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 call_user_func(
     function ($extKey) {
 
